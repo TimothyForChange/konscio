bb892a665a144b1a1db2e398e5a78df66c59df14
mooship <brits@fastmail.com>
2025-03-27 15:52:42 +0200

    Integration and favicon changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config'
 
+import sitemap from '@astrojs/sitemap'
+
+import playformCompress from '@playform/compress'
+
+import playformInline from '@playform/inline'
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [sitemap(), playformCompress(), playformInline()]
+})
