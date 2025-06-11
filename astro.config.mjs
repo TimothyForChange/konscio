65ee0038b2054ed383b0c236ce60831ce9019086
Timothy Brits <timothybrits@icloud.com>
2025-06-11 07:19:11 +0200

    Remove comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import sitemap from '@astrojs/sitemap'
 import playformCompress from '@playform/compress'
 import playformInline from '@playform/inline'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), playformCompress(), playformInline()],
   site: 'https://timothybrits.com/'
